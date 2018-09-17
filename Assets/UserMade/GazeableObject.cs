0726f8d56054b74a7085958af1d069cc15bada4b
Angshuman Mazumdar <binaryvectorjr@gmail.com>
2018-09-17 13:17:16 +0530

    Week 4 End
    All manipulaton systems done. Seamless transition b/w locomotion and manipulation.

--- Assets/UserMade/GazeableObject.cs
+++ Assets/UserMade/GazeableObject.cs
@@ -9,6 +9,11 @@ public class GazeableObject : MonoBehaviour
     private int ObjectLayer;
     private const int IGNORE_RAYCAST_LAYER = 2;
 
+    private Vector3 InitialCameraRotation;
+    private Vector3 InitialObjectRotation;
+
+    private Vector3 InitialObjectScale;
+
     public virtual void OnGazeEnter(RaycastHit HitInfo)
     {
         Debug.Log("Gaze entered on " + gameObject.name);
@@ -31,8 +36,14 @@ public class GazeableObject : MonoBehaviour
         if (IsTranformable)
         {
             ObjectLayer = gameObject.layer;
-
             gameObject.layer = IGNORE_RAYCAST_LAYER;
+
+            //Get initial rotation values
+            InitialCameraRotation = Camera.main.transform.rotation.eulerAngles;
+            InitialObjectRotation = transform.rotation.eulerAngles;
+
+            //Get initial scale
+            InitialObjectScale = transform.localScale;
         }
     }
 
@@ -89,11 +100,59 @@ public class GazeableObject : MonoBehaviour
 
     public virtual void GazeRotate(RaycastHit HitInfo)
     {
+        float RotationSpeed = 5.0f;
 
+        Vector3 CurrentCameraRotation = Camera.main.transform.rotation.eulerAngles; //Unity stores rotations in default Quarternian format - gotta convert to Vector3 for real life x,y,z values
+        Vector3 CurrentObjectRotation = transform.rotation.eulerAngles;
+
+        //Calculate how many degrees we've moved our head after pressing button
+        Vector3 DeltaRotation = CurrentCameraRotation - InitialCameraRotation;
+
+        //Now to set the rotation of object
+        Vector3 NewRotation = new Vector3(CurrentObjectRotation.x, InitialObjectRotation.y + (DeltaRotation.y * RotationSpeed), CurrentObjectRotation.z);
+
+        //Set the rotation in Quarternian terms
+        transform.rotation = Quaternion.Euler(NewRotation);
     }
 
     public virtual void GazeScale(RaycastHit HitInfo)
     {
+        float ScaleSpeed = 0.1f;
+
+        float ScaleFactor = 1.0f;
+
+        Vector3 CurrentCameraRotation = Camera.main.transform.rotation.eulerAngles;
+        Vector3 DeltaRotation = CurrentCameraRotation - InitialCameraRotation;
+
+        if (DeltaRotation.x < 0 && DeltaRotation.x > -180.0f || DeltaRotation.x > 180.0f && DeltaRotation.x < 360.0f)
+        {
+            if (DeltaRotation.x > 180.0f)   //To strictly put the range between -180 and 180
+            {
+                DeltaRotation.x = 360.0f - DeltaRotation.x;
+            }
+
+            //Looking up
+            ScaleFactor = 1.0f + Mathf.Abs(DeltaRotation.x) * ScaleSpeed;   //To keep the range between 0 and 180 degrees
+
+        }
+        else
+        {
+            if (DeltaRotation.x < -180.0f)
+            {
+                DeltaRotation.x = 360.0f + DeltaRotation.x;
+            }
+
+            //Looking down
+            //(Mathf.Abs(DeltaRotation.x) * ScaleSpeed) gives value between 0 and 180; we divide that by 180 to get
+            //a normalized value b/w 0 and 1, so that 1-x returns a positive number
+            //Then we compare whichever one between 0.1 and the calculated value is larger, as we do not want to scale
+            //below 0.1 i.e. negative values
+
+            //1/Scalespeed used coz scalespeed is a small no. and after a point the shrinking becomes to slow to be noticeable
+            ScaleFactor = Mathf.Max(0.1f, 1.0f - ((Mathf.Abs(DeltaRotation.x) * (1.0f / ScaleSpeed)) / 180.0f));
+        }
+
+        transform.localScale = ScaleFactor * InitialObjectScale;    //Finally set the scale of the object
 
     }
 }
